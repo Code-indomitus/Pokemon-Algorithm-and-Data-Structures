8ff4c782e72eb58d5f3a71eb6aeeed6698d60929
rachit <rbha0031@student.monash.edu>
2022-09-12 14:26:25 +0800

    pushing random file to test local repo status

---
+++ IntegerSum.asm
@@ -0,0 +1,21 @@
+        .data
+        # All the variables and strings go here
+
+        .text
+        # Here is where we start executing the program
+
+        # print "Enter two integers"
+
+        # take input for integer1
+
+        # take input for integer2
+
+        # add the two integers
+
+        # print 'Sum is'
+
+        # print the sum
+
+        # print newline
+
+        # exit the code
